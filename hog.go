908cfe87806a8f66a195f5f818cae970e4d0cb0d
satojkovic <satojkovic@gmail.com>
2014-10-11 23:27:35 +0900

    Add initialization step

--- hog.go
+++ hog.go
@@ -13,21 +13,22 @@ const (
 )
 
 type Cell struct {
-	Width, Height int
+	Width, Height int // the number of pixels in a cell
 	NumGrad       int
 }
 
 type Block struct {
-	Width, Height int
-	NumX, NumY    int
+	Width, Height int // the number of cells in a block
 	NumDim        int
-	Cells         []Cell
+	Cells         []Cell // cells in a block
 }
 
 type HoG struct {
-	NumX, NumY int
-	Blocks     []Block
-	Descriptor []float64
+	NumCellX, NumCellY   int // the number of cells in an image
+	NumBlockX, NumBlockY int // the number of blocks in an image
+	NumDim               int
+	Blocks               []Block // blocks in an image
+	Descriptor           []float64
 }
 
 func NewHoG(imgw, imgh int) *HoG {
@@ -37,12 +38,38 @@ func NewHoG(imgw, imgh int) *HoG {
 }
 
 func (self *HoG) Initialize(imgw, imgh int) {
+	cell := &Cell{}
+	cell.Initialize()
+	self.NumCellX = imgw / cell.Width
+	self.NumCellY = imgh / cell.Height
+	fmt.Printf("The number of pixels in a cell: (%d, %d)\n",
+		cell.Width, cell.Height)
+	fmt.Printf("The number of cells in an image: (%d, %d)\n",
+		self.NumCellX, self.NumCellY)
+
+	block := &Block{}
+	block.Initialize(imgw, imgh, cell.NumGrad)
+	self.NumBlockX = self.NumCellX - block.Width + 1
+	self.NumBlockY = self.NumCellY - block.Height + 1
+	fmt.Printf("The number of cells in a block: (%d, %d)\n",
+		block.Width, block.Height)
+	fmt.Printf("The number of blocks in an image: (%d, %d)\n",
+		self.NumBlockX, self.NumBlockY)
+
+	self.NumDim = self.NumBlockX * self.NumBlockY * block.NumDim
+	fmt.Printf("Total dimensions: %d\n", self.NumDim)
 }
 
-func (self *Block) Initialize(imgw, imgh int) {
+func (self *Block) Initialize(imgw, imgh, celldim int) {
+	self.Width = 3
+	self.Height = 3
+	self.NumDim = self.Width * self.Height * celldim
 }
 
-func (self *Cell) Initialize(imgw, imgh int) {
+func (self *Cell) Initialize() {
+	self.Width = 5
+	self.Height = 5
+	self.NumGrad = 9
 }
 
 func (hog *HoG) Extract() error {
